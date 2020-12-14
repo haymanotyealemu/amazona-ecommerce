07627d66028d2c1daede170118c4712e6271e59f
Haymanot W <haymanotalemu21@gmail.com>
2020-12-14 18:02:21 -0500

    product screen started

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,16 @@ app.get('/', (req, res)=> {
 });
 app.get('/api/products', (req, res) => {
     res.send(data.products);
-})
-const port = process.env.PORT || 7000;
+});
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if(product){
+        res.send(product);
+    } else {
+        res.status(404).send({ message: 'Product not Found'});
+    }
+});
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server at http://localhost:${port}`);
 });
