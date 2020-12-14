cb54f0aca31d81f7ff2df99e5e258e9d188cb0a4
Haymanot W <haymanotalemu21@gmail.com>
2020-12-14 18:42:56 -0500

    starting the frontend code

--- frontend/src/components/LoadingBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Loadingbox() {
-    return (
-        <div className="loading">
-            <i className="fa fa-spinner fa-spin"></i> Loading...
-        </div>
-    );
-}
