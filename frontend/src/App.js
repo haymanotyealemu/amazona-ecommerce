be0c9f2c6a416a7e6c78fb8bf97a5ccf09a150f9
Haymanot W <haymanotalemu21@gmail.com>
2020-12-16 19:19:10 -0500

    add items to cart screen done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import { useSelector} from 'react-redux';
 // import data from '../../backend/data';
 import { BrowserRouter , Route} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 function App() {
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart;
     return (
         <BrowserRouter>
             <div className="grid-container">
                 <header className="row">
                     <div>
-                        <a className="brand"href="/">amazona</a>
+                        <Link className="brand" to="/">amazona</Link>
                     </div>
                     <div>
-                        <a href="/cart">
+                        <Link to="/cart">
                             Cart
-                        </a>
-                        <a href="/signin">
+                            {cartItems.length > 0 && (
+                                <span className="badge">{cartItems.length}</span>
+                            )}
+                        </Link>
+                        <Link to="/signin">
                             Sign In
-                        </a>
+                        </Link>
                     </div>
                 </header>
                 <main>
