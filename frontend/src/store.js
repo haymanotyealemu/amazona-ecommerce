0029cb6ededbe7805b5d8c2bcbe1ce886adca816
Haymanot W <haymanotalemu21@gmail.com>
2020-12-28 14:42:33 -0500

    signin screen done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,12 @@
 import {createStore, compose, applyMiddleware, combineReducers} from "redux";
 import thunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducers";
+import { userSigninReducer } from "./reducers/userReducers";
 import { productDetailsReducer, productListReducer} from './reducers/productReducers';
 const initialState = {
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo')? JSON.parse(localStorage.getItem('userInfo')) : null
+    },
     cart: {
         cartItems: localStorage.getItem('cartItems')
             ? JSON.parse(localStorage.getItem('cartItems'))
@@ -13,7 +17,8 @@ const initialState = {
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userSignin: userSigninReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
